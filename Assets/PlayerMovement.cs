84fa905e8ff2c8caa6317456a2f175a0b715f1e9
MaciejDudzik <mr.maciekson@gmail.com>
2017-11-21 14:06:47 +0100

    First Scene + PlayerMovement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float rotationSpeed = 100.0F;
+    public float movementSpeed = 5.0F;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float z = Input.GetAxis("Vertical") * movementSpeed;
+        float x = Input.GetAxis("Horizontal") * rotationSpeed;
+        x *= Time.deltaTime;
+        z *= Time.deltaTime;
+
+        transform.Translate(0, 0, z);
+        transform.Rotate(0, x, 0);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
